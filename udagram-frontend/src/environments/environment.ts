218a97d5ace9b583f156b6fbc1bfbafd1525e615
christoli <christoliwonegou@gmail.com>
2022-11-30 06:12:08 +0000

    reverseproxy external IP address added. Udpdated .travis.yml file for frontent building with v3e tag.

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: 'Udagram Microservices App',
+  apiHost: 'http://ae967c4b5704d41b0b4e2f293d1aa8d7-1924209463.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
